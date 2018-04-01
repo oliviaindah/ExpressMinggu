03e290ad1a77f4c4fa5d9db8d036d3202c6d81c9
APPLE <apple@APPLEs-MacBook-Air.local>
2018-04-01 14:23:10 +0700

    add this as ExpressMinggu modul on Github

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ const app = Express()
 const router = require ("./config/route")
 
 // add router to express
-app.use("/", router)
+app.use("/api", router)
+
+//set express to running on port:3000
+const server = app.listen(app.get ("port"), ()=> {
+    const port1 = server.address.port 
+
+    console.log("Magic happen to port: ${port}")
+})
 
  //set express to running on port 3000
 app.listen(3000)
+
 
 // // get root request
 // app.get('/',(request, respond) =>
